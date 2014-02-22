65d97bce4895f3d87979aa7237cf9f028dcecd72
James Calfee <james@jcalfee.info>
2014-02-22 15:28:29 -0500

    install notes

--- configure.sh
+++ configure.sh
@@ -1,3 +1,12 @@
+
+set -o errexit 
+set -o xtrace
+
+sudo apt-get install npm
 pushd src/webext
 test -d node_modules/connect || npm install connect
 popd
+
+# ... Java 7
+# wget https://oss.sonatype.org/content/repositories/snapshots/org/projectodd/nodyn-standalone/0.1.1-SNAPSHOT/nodyn-standalone-0.1.1-20140220.153732-3-shaded.jar
+# java -jar nodyn-standalone-0.1.1-20140220.153732-3-shaded.jar
