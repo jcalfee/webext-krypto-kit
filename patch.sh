08ef86cd9f38a7771f2cbbabb6e73a56dae5bf75
James Calfee <james@jcalfee.info>
2014-02-25 16:23:44 -0500

    wip

--- patch.sh
+++ patch.sh
@@ -5,11 +5,11 @@
 # Re-reference chrome API to local implementation (see webext/chrome-overrides.js)
 ls src/*.js|xargs sed -i 's/[^_]chrome\./_chrome./g'
 
-# bitcoinaverage will seend a permanent forward response to same path but ending with a /
-# dataType: json added
+# bitcoinaverage will send a permanent forward response to same path but ending with a /
+# dataType: "json" was required in my testing, so results will be an object literal instead of a string 
 ls src/*.js|xargs sed -i 's#url: "https://api.bitcoinaverage.com/ticker/" + currency,#url: "https://api.bitcoinaverage.com/ticker/" + currency + "/", dataType: "json",#g'
 
-# enable blockchain's cross-origin resource sharing (cors)
+# add blockchain's cross-origin resource sharing (cors) parameter
 ls src/*.js|xargs sed -i 's|https://blockchain.info/unspent?address=|https://blockchain.info/unspent?cors=true\&address=|g'
 ls src/*.js|xargs sed -i "s|https://blockchain.info/pushtx'|https://blockchain.info/pushtx?cors=true'|g"
 
